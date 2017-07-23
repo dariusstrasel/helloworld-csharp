063b0d5a7ec32b671924f2eb0d0df229185cf3ec
Darius Strasel <strasel.darius@gmail.com>
2017-07-23 17:30:30 -0400

    Add variable examples.

--- helloworld-csharp/Program.cs
+++ helloworld-csharp/Program.cs
@@ -7,6 +7,18 @@ namespace helloworldcsharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world.");
+            byte number = 5;
+            int count = 2;
+            float cash = 3.4f;
+            char character = 'A';
+            string firstName = "Darius";
+            bool isWorking = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(cash);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
         }
     }
 }
