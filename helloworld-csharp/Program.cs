402996e9f4207d2b9eb579ae093b829c4acca4b6
Darius Strasel <strasel.darius@gmail.com>
2017-07-23 19:00:27 -0400

    Add TypeConversion(), Operators(), TryExceptCatch(); refactor code into methods, according to tutorial section.

--- helloworld-csharp/Program.cs
+++ helloworld-csharp/Program.cs
@@ -6,19 +6,91 @@ namespace helloworldcsharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world.");
-            byte number = 5;
-            int count = 2;
-            float cash = 3.4f;
-            char character = 'A';
-            string firstName = "Darius";
-            bool isWorking = true;
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(cash);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isWorking);
+            Primitives();
+            TypeConversion();
+            Operators();
+            TryExceptCatch();
+
+        }
+        static void Primitives()
+        {
+			// Hello World
+			Console.WriteLine("Hello world.");
+
+			// Variable Types
+			// Primitives
+			byte number = 5;
+			int count = 2;
+			float cash = 3.4f; // Floats have 'f' on end to indicate they be a float, as opposed to a double.
+			char character = 'A'; // Single quotes for char data types.
+			bool isWorking = true;
+			// Not primitive
+			string firstName = "Darius"; // Double quotes for string data types. Note: string is not a primitive
+
+			// Stdout printing...
+			Console.WriteLine(number);
+			Console.WriteLine(count);
+			Console.WriteLine(cash);
+			Console.WriteLine(character);
+			Console.WriteLine(firstName);
+			Console.WriteLine(isWorking);
+
+			// Template Strings:
+			Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+		}
+        static void TypeConversion()
+        {
+			// Constants
+			const float Pi = 3.14f;
+			Console.WriteLine(Pi);
+
+			// Implicit typing:
+			var implicitType = "banana";
+			Console.WriteLine(implicitType);
+
+			// Implicit type conversion
+			byte b = 1; // 00000001
+			int i = b; // 00000000 00000000 00000000 00000001
+
+			int i_copy = 1;
+			float f = i_copy;
+
+			// Explicit type conversion
+			int i_copy_2 = 1;
+			byte b_copy = (byte)i_copy_2; // casting int to byte
+
+			float f_copy = 1.0f;
+			int i_copy_3 = (int)f_copy; // float to byte, there IS data loss
+
+			string s_copy = "1";
+			// The following line won't compile, because string > int does not work
+			// int i_copy_4 = (int)s_copy;
+
+			// Can solve using Convert class, or Parse method:
+			// Convert: .net framework method, converts to .net IL data type
+			// Includes: ToByte(), ToInt16(), ToInt32(), ToInt64()
+			// Parse: 
+			int i_copy_4 = Convert.ToInt32(s_copy); // Int32 means '32' bytes, or a primitive integer
+			int j = int.Parse(s_copy);
+        }
+        static void Operators()
+        {
+            //
+            Console.WriteLine("Second program.");
+        }
+        static void TryExceptCatch()
+        {
+			// Try / Catch exception block.
+			try
+			{
+				var number_copy = "1234";
+				byte b_copy_2 = Convert.ToByte(number_copy);
+				Console.WriteLine(b_copy_2);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("The number could not be converted to a byte.");
+			}
         }
     }
 }
