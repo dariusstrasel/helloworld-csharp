a5173c61f70f707a0565aede76c32053871f9d66
Darius Strasel <strasel.darius@gmail.com>
2017-07-23 20:10:00 -0400

    Add struct RgbColor example.

---
+++ helloworld-csharp/NonPrimitives/RgbColor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace helloworldcsharp.NonPrimitives
+{
+    public struct RgbColor 
+    /* Structs are similar to classes, but more efficient. Use these for small
+    objects such as a X/Y coordinate, or RGB as mentioned here.*/
+
+    {
+        public int Red;
+        public int Green;
+        public int Blue;
+    }
+}
