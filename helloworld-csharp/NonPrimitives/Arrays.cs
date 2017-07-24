7a2d1ea071da7d5e358bb817da85efe1cfce2bd0
Darius Strasel <strasel.darius@gmail.com>
2017-07-23 20:16:55 -0400

    Add arrays example.

---
+++ helloworld-csharp/NonPrimitives/Arrays.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace helloworldcsharp.NonPrimitives
+{
+    public class Arrays
+    {
+        public void Main(string[] args)
+        {
+            var numbers = new int[3]; 
+            // Access array elements by passing index to braces
+            // All primitives inside array must be the same data type.
+            numbers[0] = 1;
+
+            // Object initilization syntax:
+            var names = new string[3] { "Jack", "John", "Mary" };
+
+        }
+    }
+}
