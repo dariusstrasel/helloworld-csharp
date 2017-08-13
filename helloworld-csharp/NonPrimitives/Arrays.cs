97a2ba7b724aaf461e8a74091ad5be239b475ddc
Darius Strasel <strasel.darius@gmail.com>
2017-08-13 12:51:54 -0400

    Adjust Main namespace.

--- helloworld-csharp/NonPrimitives/Arrays.cs
+++ helloworld-csharp/NonPrimitives/Arrays.cs
@@ -3,7 +3,7 @@ namespace helloworldcsharp.NonPrimitives
 {
     public class Arrays
     {
-        public void Main(string[] args)
+        public void program(string[] args)
         {
             var numbers = new int[3]; 
             // Access array elements by passing index to braces
@@ -13,6 +13,52 @@ namespace helloworldcsharp.NonPrimitives
             // Object initilization syntax:
             var names = new string[3] { "Jack", "John", "Mary" };
 
+            // Arrays Part 2:
+
+            var numbers2 = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            // Length
+            Console.WriteLine(numbers2.Length);
+
+            // IndexOf()
+            Console.WriteLine(Array.IndexOf(numbers2, 9));
+
+            // Clear
+            Array.Clear(numbers2, 0, 2); // 0: array, 1: index to start, 2: number of items to remove
+
+            Console.WriteLine("Effect of Array.Clear()");
+            foreach (var n in numbers2)
+            {
+                Console.WriteLine(n);
+            }
+
+            // Copy
+            Console.WriteLine("Effect of Array.Copy()");
+
+            var another = new int[3];  
+            Array.Copy(numbers2, numbers, 3);
+
+            foreach (var n in another)
+            {
+                Console.WriteLine(n);
+            }
+
+            // Sort
+            Array.Sort(numbers2);
+
+            Console.WriteLine("Effect of Array.Sort()");
+            foreach(var n in numbers2)
+            {
+                Console.WriteLine(n);
+            }
+
+            // Reverse
+            Array.Reverse(numbers2);
+
+            foreach (var n in numbers2)
+            {
+                Console.WriteLine(n);
+            }
         }
     }
 }
