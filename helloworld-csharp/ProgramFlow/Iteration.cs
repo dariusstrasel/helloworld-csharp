c57bd484c99f8bf0bb51754ab5ac45b8c4498031
Darius Strasel <strasel.darius@gmail.com>
2017-08-13 12:50:05 -0400

    Adjust Main namespace.

--- helloworld-csharp/ProgramFlow/Iteration.cs
+++ helloworld-csharp/ProgramFlow/Iteration.cs
@@ -1,10 +1,68 @@
 ﻿using System;
-namespace helloworldcsharp.ProgramFlow
+namespace Iteration
 {
     public class Iteration
     {
-        public Iteration()
+        public static void program()
         {
+            //For Loops
+            for (var i = 1; i <= 10; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+			for (var i = 10; i >= 1; i--)
+			{
+				if (i % 2 == 0)
+				{
+					Console.WriteLine(i);
+				}
+			}
+
+            // Foreach
+
+            var name = "John Smith";
+
+            // normal loop
+
+            for (var i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            // foreach loop
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+            // array foreach
+
+            var numbers = new int[] { 1, 2, 3, 4 };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            // while loop
+
+            while (true)
+            {
+                Console.Write("Write your name:");
+                var input = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+                else {
+                    Console.WriteLine(String.Format("@Echo: {0}", input));
+                }
+            }
         }
     }
 }
